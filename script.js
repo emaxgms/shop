6e39b65a82233fa35cd68cb0c0e7f054f250cbd3
Emanuele Vacca <emaxgms@gmail.com>
2025-01-20 22:27:11 +0100

    Dynamic Cloning Products

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productContainer = document.getElementById('product-container');
-    fetch('products.csv').then(response => response.text()).then(data => {
+	const productTemplate = document.querySelector('[data-product-template]');
+    fetch('prodotti.csv').then(response => response.text()).then(data => {
         const products = parseCSV(data);
         displayProducts(products);
     }).catch(error => console.error('Error loading products:', error));
 
     function parseCSV(csv) {
-        const lines = csv.split('\n');
+        const lines = csv.split('\n').filter((line) => line.trim() !== '');
         return lines.map(line => {
             const [title, description, price, image, link] = line.split(',');
             return {
@@ -21,19 +22,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function displayProducts(products) {
         products.forEach(product => {
-            const card = createProductCard(product);
-            productContainer.appendChild(card);
+			const card = productTemplate.cloneNode(true); // Clona il template
+			card.removeAttribute('hidden'); // Rendi visibile la card
+			card.querySelector('.product-title').textContent = product.title;
+			card.querySelector('.product-description').textContent = product.description;
+			card.querySelector('.product-price').textContent = product.price;
+			card.querySelector('.product-image').src = encodeURI(product.image);
+			card.querySelector('.product-image').alt = product.title;
+			card.querySelector('.product-link').href = product.link;
+
+			productContainer.appendChild(card); // Aggiungi la card al contenitore
+            // const card = createProductCard(product);
+            // productContainer.appendChild(card);
         });
     }
 
     function createProductCard(product) {
         const card = document.createElement('div');
         card.className = 'product-card';
+		console.log('RAW: ' + product.image);
+		console.log('encodato: ' + encodeURI(product.image));
+		const imgContainer = document.createElement("div");
+		imgContainer.className = "product-image-container";
         const img = document.createElement('img');
-        img.src = product.image;
+        img.src = encodeURI(product.image);
         img.alt = product.title;
         img.className = 'product-image';
         img.addEventListener('click', () => window.open(product.link, '_blank'));
+		imgContainer.appendChild(img);
         const info = document.createElement('div');
         info.className = 'product-info';
         const title = document.createElement('h2');
