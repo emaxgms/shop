1da38218582830591824a3c3efd391a2093afc8f
Emanuele Vacca <emaxgms@gmail.com>
2025-01-23 00:46:04 +0100

    Templating Component
    Uso of HTML templates to define reusable and repetable components.

--- script.js
+++ script.js
@@ -29,43 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			card.querySelector('.product-price').textContent = product.price;
 			card.querySelector('.product-image').src = encodeURI(product.image);
 			card.querySelector('.product-image').alt = product.title;
-			card.querySelector('.product-link').href = product.link;
+			card.querySelector('.product-info').href = product.link;
 
 			productContainer.appendChild(card); // Aggiungi la card al contenitore
-            // const card = createProductCard(product);
-            // productContainer.appendChild(card);
         });
     }
 
-    function createProductCard(product) {
-        const card = document.createElement('div');
-        card.className = 'product-card';
-		console.log('RAW: ' + product.image);
-		console.log('encodato: ' + encodeURI(product.image));
-		const imgContainer = document.createElement("div");
-		imgContainer.className = "product-image-container";
-        const img = document.createElement('img');
-        img.src = encodeURI(product.image);
-        img.alt = product.title;
-        img.className = 'product-image';
-        img.addEventListener('click', () => window.open(product.link, '_blank'));
-		imgContainer.appendChild(img);
-        const info = document.createElement('div');
-        info.className = 'product-info';
-        const title = document.createElement('h2');
-        title.className = 'product-title';
-        title.textContent = product.title;
-        const description = document.createElement('p');
-        description.className = 'product-description';
-        description.textContent = product.description;
-        const price = document.createElement('p');
-        price.className = 'product-price';
-        price.textContent = product.price;
-        info.appendChild(title);
-        info.appendChild(description);
-        info.appendChild(price);
-        card.appendChild(img);
-        card.appendChild(info);
-        return card;
-    }
 });
